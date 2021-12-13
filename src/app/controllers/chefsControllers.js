9ac93f5c72030c116522d141a1111322b05eeab2
Gustavo Litter <gustavolitter@gmail.com>
2021-12-12 23:08:23 -0300

    chore: try/catch in chefs page

--- src/app/controllers/chefsControllers.js
+++ src/app/controllers/chefsControllers.js
@@ -4,122 +4,157 @@ const File = require('../models/fileModels')
 
 module.exports = {
     async index(req, res) {
-        let { page, limit } = req.query
-        page = page || 1
-        limit = limit || 12
-        let offset = limit * (page - 1)
+        try {
+            let { page, limit } = req.query
+            page = page || 1
+            limit = limit || 12
+            let offset = limit * (page - 1)
 
-        const params = {
-            page,
-            limit,
-            offset
-        }
-        const chefPaginate = await Chef.paginate(params)
-        const chefs = chefPaginate.rows
-
-        if (chefs[0] != null) {
-            const pagination = {
-                total: Math.ceil(chefs[0].total / limit),
+            const params = {
                 page,
+                limit,
+                offset
             }
-            return res.render('admin/chefs/index', { chefs, pagination })
+            const chefPaginate = await Chef.paginate(params)
+            const chefs = chefPaginate.rows
+
+            if (chefs[0] != null) {
+                const pagination = {
+                    total: Math.ceil(chefs[0].total / limit),
+                    page,
+                }
+                return res.render('admin/chefs/index', { chefs, pagination })
+            }
+            return res.render('admin/chefs/index', { chefs })
+
+        } catch (err) {
+            console.error(err)
         }
-        return res.render('admin/chefs/index', { chefs })
     },
 
     async create(req, res) {
-        return res.render('admin/chefs/create')
+        try {
+            return res.render('admin/chefs/create')
+            
+        } catch (err) {
+            console.error(err)
+        }
     },
     
     async post(req, res) {
-        const keys = Object.keys(req.body)
+        try {
+            const keys = Object.keys(req.body)
 
-        for ( let key of keys ) {
-            if (req.body[key] == '') {
-                return res.send('Preencha todos os campos')
+            for ( let key of keys ) {
+                if (req.body[key] == '') {
+                    return res.send('Preencha todos os campos')
+                }
             }
+
+            if (req.files.length == 0) {
+                return res.send('por favor, adicione uma imagem')
+            }
+
+            const { filename, path } = req.files[0]
+            const file_idResults = await File.create({ name: filename, path })
+            
+            const file_id = file_idResults.rows[0].id
+            const { name } = req.body
+            const results = await Chef.create({ name, file_id })
+
+            return res.redirect(`/admin/chefs/${results.rows[0].id}`)
+
+        } catch (err) {
+            console.error(err)
         }
-
-        if (req.files.length == 0) {
-            return res.send('por favor, adicione uma imagem')
-        }
-
-        const { filename, path } = req.files[0]
-        const file_idResults = await File.create({ name: filename, path })
-        
-        const file_id = file_idResults.rows[0].id
-        const { name } = req.body
-        const results = await Chef.create({ name, file_id })
-
-        return res.redirect(`/admin/chefs/${results.rows[0].id}`)
     },
 
     async show(req, res) {
-        const id = req.params.id
+        try {
+            const id = req.params.id
 
-        const chefResults = await Chef.find(id)
-        const chef = chefResults.rows[0]
-            
-        if ( !chef ) return res.send('chef not found')
+            const chefResults = await Chef.find(id)
+            const chef = chefResults.rows[0]
+                
+            if ( !chef ) return res.send('chef not found')
 
-        let { page, limit } = req.query
+            let { page, limit } = req.query
 
-        page = page || 1
-        limit = limit || 6
-        let offset = limit * (page - 1)
+            page = page || 1
+            limit = limit || 6
+            let offset = limit * (page - 1)
 
-        const params = {
-            id,
-            limit,
-            offset
-        }
-        const recipesResults = await Recipe.recipeFind(params)
-        const recipes = recipesResults.rows
-
-        if (recipes[0] != null) {
-            const pagination = {
-                total: Math.ceil(recipes[0].total / limit),
-                page,
+            const params = {
+                id,
+                limit,
+                offset
             }
-            return res.render('admin/chefs/show', { chef, recipes, pagination })
+            const recipesResults = await Recipe.recipeFind(params)
+            const recipes = recipesResults.rows
+
+            if (recipes[0] != null) {
+                const pagination = {
+                    total: Math.ceil(recipes[0].total / limit),
+                    page,
+                }
+                return res.render('admin/chefs/show', { chef, recipes, pagination })
+            }
+            return res.render('admin/chefs/show', { chef, recipes })
+        
+        } catch (err) {
+            console.error(err)
         }
-        return res.render('admin/chefs/show', { chef, recipes })
     },
 
     async edit(req, res) {
-        let chefFind = await Chef.find(req.params.id)
-        const chef = chefFind.rows[0]
+        try {
+            let chefFind = await Chef.find(req.params.id)
+            const chef = chefFind.rows[0]
 
-        if (!chef) return res.send('chef not found')
+            if (!chef) return res.send('chef not found')
 
-        chefFind = await Chef.files(chef.file_id)
-        let files = chefFind.rows[0]
-        
-        return res.render('admin/chefs/edit', { chef, files })
+            chefFind = await Chef.files(chef.file_id)
+            let files = chefFind.rows[0]
+            
+            return res.render('admin/chefs/edit', { chef, files })
+                
+        } catch (err) {
+            console.error(err)
+        }
     },
 
     async put(req, res) {
-        const keys = Object.keys(req.body)
+        try {
+             const keys = Object.keys(req.body)
 
-        for ( let key of keys ) {
-            if (req.body[key] == '') return res.send('Preencha todos os campos')
-        }
+            for ( let key of keys ) {
+                if (req.body[key] == '') return res.send('Preencha todos os campos')
+            }
 
-        await Chef.update(req.body)
+            await Chef.update(req.body)
 
-        return res.redirect(`/admin/chefs/${req.body.id}`)
+            return res.redirect(`/admin/chefs/${req.body.id}`)
+           
+        } catch (err) {
+            console.error(err)
+       }
     },
 
     async delete(req, res) {
-        const id = req.body.id
-        const findChef = await Chef.find(id)
-        const chef = findChef.rows[0]
+        try {
+             const id = req.body.id
+            const findChef = await Chef.find(id)
+            const chef = findChef.rows[0]
 
-            if (chef.total_recipes == 0) {
-                Chef.delete(id)
-                return res.redirect(`/admin/chefs`)
-            } else {
-                return res.redirect(`/admin/chefs/${id}/edit`)
-            }
+                if (chef.total_recipes == 0) {
+                    Chef.delete(id)
+                    return res.redirect(`/admin/chefs`)
+                } else {
+                    return res.redirect(`/admin/chefs/${id}/edit`)
+                }
+           
+        } catch (err) {
+            console.error(err)
+       }
     },
 }
